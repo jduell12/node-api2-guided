ec78dee3f7289b187efbaf2c98e845a709bee9ed
Jessica <duellje@oregonstate.edu>
2020-08-04 13:12:43 -0400

    formatted code

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const hubsRouter = require('./hubs/hubs-router')
-const clientsRouter = require('./routers/clientsRouter')
-const productsRouter = require('./routers/productsRouter')
-const orderRouter = require('./routers/orderRoutes')
-const suppliersRouter = require('./routers/suppliersRouter')
+const express = require("express");
+const hubsRouter = require("./hubs/hubs-router");
+const clientsRouter = require("./routers/clientsRouter");
+const productsRouter = require("./routers/productsRouter");
+const orderRouter = require("./routers/orderRoutes");
+const suppliersRouter = require("./routers/suppliersRouter");
 
-const server = express()
-server.use(express.json())
+const server = express();
+server.use(express.json());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`
     <h2>Lambda Hubs API</h>
     <p>Welcome to the Lambda Hubs API</p>
-  `)
-})
-server.use('/api/hubs', hubsRouter)
-server.use('/clients', clientsRouter)
-server.use('/products', productsRouter)
-server.use('/orders', orderRouter)
-server.use('/suppliers', suppliersRouter)
+  `);
+});
+server.use("/api/hubs", hubsRouter);
+server.use("/clients", clientsRouter);
+server.use("/products", productsRouter);
+server.use("/orders", orderRouter);
+server.use("/suppliers", suppliersRouter);
 
 // add an endpoint that returns all the messages for a hub
 // add an endpoint for adding new message to a hub
 
 server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n')
-})
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
--- routers/clientsRouter.js
+++ routers/clientsRouter.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ router: 'clients' })
-})
+router.get("/", (req, res) => {
+  res.status(200).json({ router: "clients" });
+});
 
-module.exports = router
+router.get("/:id", (req, res) => {
+  res.status(200).json({ router: `results for id ${req.params.id}` });
+});
+
+module.exports = router;
